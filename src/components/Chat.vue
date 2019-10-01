bc89b77caf24ed8b4bd504c839b845d651812a1e
chriztiaan <chriz.ek@gmail.com>
2019-10-01 23:02:02 +0200

    Started working on the chat page code.

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,5 +1,51 @@
 <template>
 	<div>
-		Chat
+		<v-toolbar flat color="orange" dark height="64">
+			<!-- <v-toolbar color="orange" dark tile> -->
+			<v-btn icon class="ml-n3" width="32" height="32">
+				<v-icon>mdi-arrow-left</v-icon>
+			</v-btn>
+
+			<v-avatar>
+				<v-img aspect-ratio="1" width="45" max-width="45" height="45" :src="avatar"></v-img>
+			</v-avatar>
+			<v-toolbar-title>
+				<v-list-item two-line class="pl-2 pr-0">
+					<v-list-item-content>
+						<v-list-item-title>{{ title }}</v-list-item-title>
+						<v-list-item-subtitle class="white--text">{{ time }}</v-list-item-subtitle>
+					</v-list-item-content>
+				</v-list-item>
+			</v-toolbar-title>
+			<v-spacer />
+
+			<v-btn icon width="40" height="40">
+				<v-icon>mdi-video</v-icon>
+			</v-btn>
+
+			<v-btn icon width="40" height="40">
+				<v-icon>mdi-phone</v-icon>
+			</v-btn>
+
+			<v-btn icon width="40" height="40">
+				<v-icon>mdi-dots-vertical</v-icon>
+			</v-btn>
+		</v-toolbar>
+		<v-sheet id="" class="overflow-y-auto" max-height="600">
+			<v-container class="pa-0">a</v-container>
+		</v-sheet>
 	</div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+	data() {
+		return {
+			avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+			title: 'Emma Landman',
+			time: 'Online'
+		};
+	}
+});
+</script>
