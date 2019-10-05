000eab5881833b8cde1c2515a42be2a0a8ed3254
chriztiaan <chriz.ek@gmail.com>
2019-10-06 00:39:00 +0200

    Add chat bubble corner, loading messages dynamically.

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -3,26 +3,42 @@
 		<ChatTopBar></ChatTopBar>
 		<v-sheet id="" class="overflow-y-auto" max-height="600">
 			<v-container class="pa-0 pt-2 grey lighten-2 back subtitle-2" style="height: 90vh;">
-				<v-card class="mx-3 mb-1 px-2 py-1" width="80%">
-					We should be able to get there on time right?
-					<span class="float-right mt-1">
-						<span class="caption grey--text text--darken-1"> {{ messages[0].time }} </span>
-					</span>
-				</v-card>
-				<v-card class="mx-3 mb-2 px-2 py-1" width="80%">
-					I mean, surely? It can't happen ag
-					<span class="float-right mt-1">
-						<span class="caption grey--text text--darken-1"> {{ messages[0].time }} </span>
-					</span>
-				</v-card>
-				<v-card class="mx-3 mb-1 px-2 py-1 float-right orange lighten-3 " width="80%">
-					Don't hold up your breathe. Todd always takes years to get ready. It will be fine either way.
-					<span class="float-right mt-1" style="height:18px;">
-						<span class="caption grey--text text--darken-1"> {{ messages[0].time }} </span>
-						<!-- <v-icon size="18" class="mt-n1" color="grey lighten-0">mdi-check-all</v-icon> -->
-						<v-icon size="18" class="mt-n1" color="blue lighten-2">mdi-check-all</v-icon>
-					</span>
-				</v-card>
+				<template v-for="(message, index) in messages">
+					<v-card
+						v-if="message.direction == 'incoming'"
+						:key="index"
+						:class="{ 'chat-bubble mt-1': firstDirection(message, index) }"
+						class="left mx-3 mb-1 px-2 py-1 float-left"
+						min-width="50%"
+						max-width="80%"
+					>
+						<div class="hide-overflow">
+							{{ message.text }}
+						</div>
+						<span class="float-right mt-1 pl-2" style="height:18px;">
+							<span class="caption grey--text text--darken-1"> {{ message.time }} </span>
+						</span>
+					</v-card>
+
+					<v-card
+						v-else-if="message.direction == 'outgoing'"
+						:key="index"
+						:class="{ 'chat-bubble mt-1': firstDirection(message, index) }"
+						class="right mx-3 mb-1 px-2 py-1 float-right orange lighten-3"
+						min-width="50%"
+						max-width="80%"
+					>
+						<div class="hide-overflow">
+							{{ message.text }}
+						</div>
+						<span class="float-right mt-1 pl-2" style="height:18px;">
+							<span class="caption grey--text text--darken-1"> {{ message.time }} </span>
+							<v-icon v-if="message.status == 'received'" size="18" class="mt-n1" color="grey lighten-0">mdi-check-all</v-icon>
+							<v-icon v-else-if="message.status == 'read'" size="18" class="mt-n1" color="blue lighten-2">mdi-check-all</v-icon>
+						</span>
+					</v-card>
+				</template>
+
 				<ChatBox></ChatBox>
 			</v-container>
 		</v-sheet>
@@ -40,12 +56,62 @@ export default Vue.extend({
 		return {
 			messages: [
 				{
+					direction: 'incoming',
+					text: `We should be able to get there on time right?`,
+					time: '8:10pm'
+				},
+				{
+					direction: 'incoming',
+					text: `I mean, surely? It can't happen again`,
+					time: '8:11pm'
+				},
+				{
 					direction: 'outgoing',
-					text: 'I am on the internet.',
+					text: `Don't hold up your breathe. Todd always takes years to get ready. It will be fine either way.`,
+					status: 'received/read',
 					time: '8:18pm'
+				},
+				{
+					direction: 'outgoing',
+					text: `We are going to be fine.`,
+					status: 'read',
+					time: '8:19pm'
+				},
+				{
+					direction: 'outgoing',
+					text: `:)`,
+					status: 'read',
+					time: '8:20pm'
+				},
+				{
+					direction: 'incoming',
+					text: 'Almost here?',
+					time: '9:00pm'
+				},
+				{
+					direction: 'incoming',
+					text: 'Is that your car in the street?',
+					time: '9:02pm'
+				},
+				{
+					direction: 'outgoing',
+					text: 'Yea.',
+					status: 'received',
+					time: '9:03pm'
 				}
-			]
+			],
+			lastMessage: {}
 		};
+	},
+	methods: {
+		firstDirection(message: any, index: number): boolean {
+			if (index == 0) {
+				return true;
+			}
+			const check: boolean = message.direction != this.messages[index - 1].direction;
+
+			return check;
+		}
 	}
 });
 </script>
@@ -54,5 +120,43 @@ export default Vue.extend({
 .back {
 	background-image: url('../assets/background.png');
 	background-size: 140% 140%;
+}
+
+.hide-overflow {
+	display: inline;
+	overflow: hidden;
+}
+
+.chat-bubble::after {
+	content: '';
+	position: absolute;
+	top: 10px;
+	width: 0;
+	height: 0;
+	border: 10px solid transparent;
+	border-top: 0;
+	margin-top: -10px;
+}
+
+.chat-bubble.left::after {
+	left: 0;
+	border-right-color: inherit;
+	border-left: 0;
+	margin-left: -10px;
+}
+
+.chat-bubble.right::after {
+	right: 0;
+	border-left-color: inherit;
+	border-right: 0;
+	margin-right: -10px;
+}
+
+.chat-bubble.left {
+	border-top-left-radius: 0px;
+}
+
+.chat-bubble.right {
+	border-top-right-radius: 0px;
 }
 </style>
